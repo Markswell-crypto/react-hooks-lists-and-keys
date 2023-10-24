18bf144647a61a1b1c08f65e86bbf10aced9cbe8
Markswell <osoromarkswell80@gmail.com>
2023-10-24 13:24:56 +0300

    Assignment complete

--- src/components/ColorList.js
+++ src/components/ColorList.js
@@ -12,19 +12,15 @@ function ColorList() {
     "green"
   ];
 
+  const colorElements = colors.map((color) => {
+    return <li key={color} style={{ color: color }}>{color}</li>;
+  });
   return (
     <div>
       <h1>Top 5 CSS Colors</h1>
       <ol>
-        <li style={{color: colors[0] }}>{colors[0]}</li>
-        <li style={{ color: colors[1] }}>{colors[1]}</li>
-        <li style={{ color: colors[2] }}>{colors[2]}</li>
-        <li style={{ color: colors[3] }}>{colors[3]}</li>
-        <li style={{ color: colors[4] }}>{colors[4]}</li>
-        <li style={{ color: colors[5] }}>{colors[5]}</li>
-        <li style={{ color: colors[6] }}>{colors[6]}</li>
-        <li style={{ color: colors[7] }}>{colors[7]}</li>
-
+        {/* display the array of <li> elements here! */}
+        {colorElements}
       </ol>
     </div>
   );
